f946a994b2ce1bc042725da1fdecf25132322201
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-08-06 17:42:06 +0300

    Add pagination

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,19 @@
 			v-if="!isPostsLoading"
 		/>
 		<div v-else>Loading...</div>
+		<div class="page__wrapper">
+			<div 
+				v-for="pageNumber in totalPages" 
+				:key="pageNumber"
+				class="page"
+				:class="{
+					'current-page': page === pageNumber
+				}"
+				@click="changePage(pageNumber)"
+			>
+				{{ pageNumber }}
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -46,6 +59,9 @@ export default {
 			isPostsLoading: false,
 			selectedSort: '',
 			searchQuery: '',
+			page: 1,
+			limit: 10,
+			totalPages: 0,
 			sortOptions: [
 				{value: 'title', name: 'По названию'},
 				{value: 'body', name: 'По содержимому'},
@@ -63,10 +79,19 @@ export default {
 		showDialog() {
 			this.dialogVisible = true;
 		},
+		changePage(pageNumber) {
+			this.page = pageNumber;
+		},
 		async fetchPosts() {
 			try {
 				this.isPostsLoading = true;
-				const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+				const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+					params: {
+						_page: this.page,
+						_limit: this.limit
+					}
+				});
+				this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
 				this.posts = response.data;
 			} catch (e) {
 				console.error('Error');
@@ -87,6 +112,9 @@ export default {
 		}
 	},
 	watch: {
+		page() {
+			this.fetchPosts();
+		}
 	}
 }
 </script>
@@ -106,5 +134,18 @@ export default {
 	margin: 15px 0;
 	display: flex;
 	justify-content: space-between;
+}
+.page__wrapper {
+	display: flex;
+	margin-top: 15px;
+}
+.page {
+	border: 1px solid black;
+	padding: 10px;
+	cursor: pointer;
+}
+.current-page {
+	border: 2px solid teal;
+	cursor: default;
 }
 </style>
