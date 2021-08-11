1aacd4dc368c1d5ff9d21a88ec0ee91cf21c6c80
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-08-11 18:14:26 +0300

    Add routing

---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>My first Vue3 app, created by course on YouTube</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+	
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>Welcome on Vue3 course</h1>
+		<router-link to="/posts" >Posts</router-link>
+		<router-link to="/about" >About</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+	
+</style>
